60ce47013810d9a61235ea20b8d1398049858d7a
shumail <shumailrafaqat2003@gmail.com>
2024-10-12 11:05:29 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Navbar scroll effect
+window.addEventListener('scroll', () => {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+      navbar.classList.add('sticky');
+    } else {
+      navbar.classList.remove('sticky');
+    }
+  });
+  
